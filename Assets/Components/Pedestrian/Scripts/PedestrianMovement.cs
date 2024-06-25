96cf6c8ba633db62c2c8c03496255363bb98fa23
Nikolay Demidovez <nikolaydemidovez@gmail.com>
2024-06-25 23:53:40 +0300

    added NavMesh

--- Assets/Components/Pedestrian/Scripts/PedestrianMovement.cs
+++ Assets/Components/Pedestrian/Scripts/PedestrianMovement.cs
@@ -1,73 +1,50 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
 
 namespace PedestrianSpace
 {
-    [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(NavMeshAgent))]
     public class PedestrianMovement : MonoBehaviour
     {
         [SerializeField] private float _minMoveSpeed = 0.75f;
         [SerializeField] private float _maxMoveSpeed = 1.5f;
-        [SerializeField] private float _stopDistance;
-        [SerializeField] private float _rotationSpeed;
         [SerializeField] private string _debugPointName;
         
-        private Vector3 _destination;
         private Pedestrian _pedestrian;
-        private CharacterController _characterController;
-        private float _movementSpeed;
+        private NavMeshAgent _navMeshAgent;
 
         public bool IsReachedDestination { get; private set; }
 
         private void Start()
         {
-            _characterController = GetComponent<CharacterController>();
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _navMeshAgent.speed = Random.Range(_minMoveSpeed, _maxMoveSpeed);
+            
             _pedestrian = GetComponent<Pedestrian>();
-            _movementSpeed = Random.Range(_minMoveSpeed, _maxMoveSpeed);
         }
 
         private void Update()
         {
-            if (_pedestrian.ShouldStopAndWait)
+            _navMeshAgent.isStopped = _pedestrian.ShouldStopAndWait;
+
+            if (_navMeshAgent.isStopped)
             {
-                _characterController.Move(Vector3.zero);
-                
-                return;
+                IsReachedDestination = false;
             }
-            
-            if (transform.position != _destination)
+            else
             {
-                Vector3 destinationDirection = _destination - transform.position;
-                destinationDirection.y = 0f;
-
-                float destinationDistance = destinationDirection.magnitude;
-
-                if (destinationDistance >= _stopDistance)
-                {
-                    IsReachedDestination = false;
-
-                    Quaternion targetRotation = Quaternion.LookRotation(destinationDirection);
-                    transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation,
-                        _rotationSpeed * Time.deltaTime);
-
-                    Vector3 movement = destinationDirection.normalized * (_movementSpeed * Time.deltaTime);
-                    movement.y = -1f;
-                    _characterController.Move(movement);
-                }
-                else
-                {
-                    IsReachedDestination = true;
-                }
+                IsReachedDestination = _navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance;
             }
         }
 
         public void SetDestination(Vector3 position, string debugPointName)
         {
-            _destination = position;
             _debugPointName = debugPointName;
 
+            _navMeshAgent.SetDestination(position);
             IsReachedDestination = false;
         }
     }
